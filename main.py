7df0787c267c291245dbc64a1a71a631148685a6
bengunning <bgunning@ash.cse.nd.edu>
2017-04-28 17:24:17 -0400

    Added tail

--- main.py
+++ main.py
@@ -2,17 +2,25 @@
 # Maddie Gleason and Ben Gunning
 # PyGame + Twisted Final Project
 
-import os, sys, pygame, math
+import os, sys, pygame, math, collections
 
 class Player(pygame.sprite.Sprite):
+# Initialize Player with Starting Position
 	def __init__(self,gs):
-		self.head = pygame.Surface((10,10))
+		self.head = pygame.Surface((5,5))
 		self.rect = self.head.get_rect()
 		self.rect.centerx = 320
 		self.rect.centery = 240
 		self.blue = (0,0,255)
 		self.xvel = 0
 		self.yvel = -1
+
+# Create Tail to Store Previous Rectangles
+		self.tail_len = 50
+		self.tail = collections.deque()
+		self.tail.appendleft(self.rect.copy())
+
+# Change the Direction of the Player
 	def move(self,key):
 		if key == pygame.K_UP and self.yvel <= 0:
 			self.xvel = 0
@@ -26,9 +34,16 @@ class Player(pygame.sprite.Sprite):
 		if key == pygame.K_RIGHT and self.xvel >= 0:
 			self.xvel = 1
 			self.yvel = 0
+
 	def tick(self):
+# Update the Player Position
 		self.rect.centerx += self.xvel
 		self.rect.centery += self.yvel
+
+# Add the New Rectangle to the Left of the Tail and Pop the Rightmost Rectangle
+		self.tail.appendleft(self.rect.copy())
+		while len(self.tail) > self.tail_len:
+			self.tail.pop()
 
 class GameSpace:
 	def main(self):
@@ -38,10 +53,6 @@ class GameSpace:
 		self.black = (0,0,0)
 		self.screen = pygame.display.set_mode(self.size)
 		self.clock = pygame.time.Clock()
-
-# We could use the following command if we want the user to hold down the key for the direction they want to move
-# This probably isn't necessary if we just have the user press a key only to change direction
-#		pygame.key.set_repeat(1,60)
 
 # Initialize Game Objects
 		self.player = Player(self)
@@ -67,7 +78,8 @@ class GameSpace:
 
 # Update Screen
 			self.screen.fill(self.black)
-			self.screen.blit(self.player.head,self.player.rect)
+			for rectangle in self.player.tail:
+				self.screen.blit(self.player.head,rectangle)
 			self.player.head.fill(self.player.blue)
 			pygame.display.flip()
 			pygame.display.update()
